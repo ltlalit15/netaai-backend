214056fad86111c74e90842ad2c9a320816e06a4
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-16 21:10:27 +0530

    Update ipRoutes.js

--- routes/ipRoutes.js
+++ routes/ipRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ipController = require('../controller/ipController');
+const ipController = require('../controller/ipcontroller');
 
 router.post('/check-ip', ipController.checkAndSaveIp);
 router.post('/increase-chat', ipController.increaseChatCount);
