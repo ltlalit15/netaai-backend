6d1cd2e0b399fc977a9a081f8e4edfe6cd89b918
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-28 17:23:12 +0530

    Update mergeRoutes.js

--- routes/mergeRoutes.js
+++ routes/mergeRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const necController = require('../controller/necController');
+const mergeController = require('../controller/mergeController');
 
-router.post('/filterNecCodeAndCopyPages', necController.filterNecCodeAndCopyPages);
-
+router.post('/mergeArticlePDFs', mergeController.mergeArticlePDFs);
 
 module.exports = router;
